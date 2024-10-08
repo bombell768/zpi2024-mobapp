5c49725704c387397c42faae4f41be1e58967aba
Bartosz Strzecha <bstrzecha@gmail.com>
2024-10-08 11:42:52 +0200

    Initial Commit

---
+++ Umow.sie/Umow_sieApp.swift
@@ -0,0 +1,17 @@
+//
+//  Umow_sieApp.swift
+//  Umow.sie
+//
+//  Created by Bartosz Strzecha on 08/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Umow_sieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
