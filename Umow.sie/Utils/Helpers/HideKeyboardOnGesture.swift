3fa64e61c81cc1d89b34ff033d335c141a22a4a6
bombell768 <266878@student.pwr.edu.pl>
2024-10-16 21:14:39 +0200

    Gesty zamykania klawiatury, poprawione pola wprowadzania, widok zmiany hasla

---
+++ Umow.sie/Utils/Helpers/HideKeyboardOnGesture.swift
@@ -0,0 +1,38 @@
+//
+//  HideKeyboardOnSwipe.swift
+//  Umow.sie
+//
+//  Created by Bartosz Strzecha on 15/10/2024.
+//
+
+import SwiftUI
+
+struct HideKeyboardOnGesture: ViewModifier {
+    @ObservedObject var keyboardManager = KeyboardManager() // Obserwujemy stan klawiatury
+
+    func body(content: Content) -> some View {
+        content
+            // Dodaj gest przeciągnięcia (swipe down)
+            .gesture(
+                DragGesture(minimumDistance: 10, coordinateSpace: .local)
+                    .onEnded { value in
+                        if value.translation.height > 0 {
+                            UIApplication.shared.hideKeyboard()
+                        }
+                    }
+            )
+            // Dodaj gest tapnięcia tylko, gdy klawiatura jest widoczna
+            .onTapGesture {
+                if keyboardManager.isKeyboardVisible {
+                    UIApplication.shared.hideKeyboard()
+                }
+            }
+    }
+}
+
+extension View {
+    // Modyfikator dla tapnięcia i swipe, kontrolujący stan klawiatury
+    func hideKeyboardOnGesture() -> some View {
+        self.modifier(HideKeyboardOnGesture())
+    }
+}
---
+++ Umow.sie/Views/ForgotPasswordView.swift
@@ -0,0 +1,18 @@
+//
+//  ForgotPasswordView.swift
+//  Umow.sie
+//
+//  Created by Bartosz Strzecha on 15/10/2024.
+//
+
+import SwiftUI
+
+struct ForgotPasswordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ForgotPasswordView()
+}
