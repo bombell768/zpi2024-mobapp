b036e3a6013180ec9079ba108189d69f3f7d226e
bombell768 <266878@student.pwr.edu.pl>
2024-12-05 02:43:53 +0100

    widok zmiany terminu wizyty, przejścia do widoku

---
+++ Umow.sie/Views/ChangeDateView.swift
@@ -0,0 +1,18 @@
+//
+//  ChangeDateView.swift
+//  Umow.sie
+//
+//  Created by Bartosz Strzecha on 05/12/2024.
+//
+
+import SwiftUI
+
+struct ChangeDateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChangeDateView()
+}
